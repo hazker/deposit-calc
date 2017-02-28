7369182b54c504b1b34d765d7f85d3af32cebeec
hazker <zov.zon@gmail.com>
2017-02-28 19:03:04 +0700

    Plus one function

--- gitulechka.cpp
+++ gitulechka.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <stdio.h>
 
+void Check(int s, int c);
+
 int main(){
 float d,s,c;
 	printf("Vvedit summu vklada(v tisachah)-\n");
 	scanf("%f",&s);
 	printf("Vvedite srok(v dnyah)-\n");
 	scanf("%f",&c);
+	Check(s,c);
+	return 0;
+}
+
+
+void Check(int s, int c){
+	if(s<10 || c>365){
+	printf("Wrong, TRY AGAIN \n");
+	main();
+}
+}
