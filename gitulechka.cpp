d02947ef4bf06c9e7c223838fd28fbb9ea4a7342
hazker <zov.zon@gmail.com>
2017-02-28 18:31:23 +0700

    Eeeee, make great again through function

--- gitulechka.cpp
+++ gitulechka.cpp
@@ -7,38 +7,3 @@ float d,s,c;
 	scanf("%f",&s);
 	printf("Vvedite srok(v dnyah)-\n");
 	scanf("%f",&c);
-	if(s<9.9 || c>365){
-		printf("WRONG");
-		return 0;
-	}
-	if(s<100){
-		if (c>30 && c<121){
-			d=s*1.02;
-		}
-		if(c>120 && c<241){
-			d=s*1.06;
-		}
-		if(c<31){
-			d=s*0.9;
-		}
-		if(c<365 && c>240){
-			d=s*1.12;
-		}
-	}
-	if(s>100){
-		if(c<31){
-			d=s*0.9;
-		}
-		if(c>30 && c<121){
-			d=s*1.03;
-		}
-		if(c>120 && c<241){
-			d=s*1.08;
-		}
-		if(c>240 && c<365){
-			d=s*1.15;
-		}
-	}
-	printf("DOXOD= %f",d);
-		return 0;
-}
